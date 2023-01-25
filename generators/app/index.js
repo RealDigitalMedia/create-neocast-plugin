beb29918b447e0e38b7197340ae72344f1eac640
Gavin Stark <gavin@gstark.com>
2023-01-25 17:22:32 -0500

    Modernize create-neocast-plugin
    - Removes yarn as a dependency, uses npm directly
      - We used yarn long ago since npm was terribly slow. This is not the
        case for modern versions of node and npm.
    - Moves away from webpack and to esbuild
      - esbuild is a simpler, and faster packaging tool
    - Updates all dependencies of the create-neocast-plugin
    - Creates a package.json in the generated plugin that has commands to
      build the plugin, see the `build` step.
    - Adds the dist directory to gitignore

--- generators/app/index.js
+++ generators/app/index.js
@@ -6,110 +6,93 @@ const chalk = require('chalk')
 const _ = require('lodash')
 
 class CreateNeocastPlugin extends Generator {
-  constructor (args, options) {
+  constructor(args, options) {
     super(args, options)
     this.destinationRoot(this.options.name)
     this.appname = this.determineAppname()
   }
 
-  async prompting () {
-    this.props = {
-    }
+  async prompting() {
+    this.props = {}
 
-    let prompts = [{
-      type: 'confirm',
-      name: 'empty',
-      message: `This directory (${chalk.blue(this.destinationRoot())}) is ${chalk.red.bold('not')} empty. Should we bail?`,
-      default: true,
-      when: () => !emptyDir.sync(this.destinationRoot())
-    }, {
-      type: 'input',
-      name: 'title',
-      message: `What's your project's title?`,
-      default: _.startCase(this.appname),
-      when: (props) => !props.empty
-    }, {
-      type: 'confirm',
-      name: 'repo',
-      message: 'Create GitHub repository?',
-      default: false,
-      when: (props) => !props.empty
-    }]
+    let prompts = [
+      {
+        type: 'confirm',
+        name: 'empty',
+        message: `This directory (${chalk.blue(this.destinationRoot())}) is ${chalk.red.bold(
+          'not'
+        )} empty. Should we bail?`,
+        default: true,
+        when: () => !emptyDir.sync(this.destinationRoot()),
+      },
+      {
+        type: 'input',
+        name: 'title',
+        message: `What's your project's title?`,
+        default: _.startCase(this.appname),
+        when: props => !props.empty,
+      },
+      {
+        type: 'confirm',
+        name: 'repo',
+        message: 'Create GitHub repository?',
+        default: false,
+        when: props => !props.empty,
+      },
+    ]
 
-    return this.prompt(prompts).then((props) => {
+    return this.prompt(prompts).then(props => {
       if (props.empty) {
         this.log(`Whew... ${chalk.green('that was a close one.')} Bye!`)
         process.exit(0)
       } else {
+        // @ts-ignore
         Object.assign(this.props, props)
       }
     })
   }
 
-  get writing () {
+  get writing() {
     return {
-      webpack () {
-        this.fs.copyTpl(
-          this.templatePath('webpack.config.js'),
-          this.destinationPath('webpack.config.js'),
-          this.props
-        )
-      },
-
-      packageJSON () {
+      packageJSON() {
         const pkg = {
-          private: true,
+          name: 'Title',
+          version: '1.0.0',
+          description: 'This is your NEOCAST Media Player Plugin.',
+          main: 'index.js',
+          scripts: {
+            run: 'node src/index.js',
+            build:
+              'esbuild src/index.js --bundle --minify --platform=node --analyze --target=node8 --outfile=dist/index.js',
+          },
+          dependencies: {},
+          author: '',
+          license: 'UNLICENSED',
         }
-
-        this.fs.writeJSON('package.json', pkg)
+        this.fs.writeJSON(this.destinationPath('package.json'), pkg)
       },
 
-      gitIgnore () {
-        this.fs.copyTpl(
-          this.templatePath('gitignore'),
-          this.destinationPath('.gitignore'),
-          this.props
-        )
+      gitIgnore() {
+        this.fs.copyTpl(this.templatePath('gitignore'), this.destinationPath('.gitignore'), this.props)
       },
 
-      scripts () {
-        this.fs.copyTpl(
-          this.templatePath('index.js'),
-          this.destinationPath('src/index.js'),
-          this.props
-        )
+      scripts() {
+        this.fs.copyTpl(this.templatePath('index.js'), this.destinationPath('src/index.js'), this.props)
       },
 
-      readme () {
-        this.fs.copyTpl(
-          this.templatePath('README.md'),
-          this.destinationPath('README.md'),
-          this.props
-        )
-      }
+      readme() {
+        this.fs.copyTpl(this.templatePath('README.md'), this.destinationPath('README.md'), this.props)
+      },
     }
   }
 
-  install () {
-    const devDependencies = [
-    ]
-
-    devDependencies.push(
-      'json-loader',
-      'babel-loader',
-      'babel-core'
-    )
-
-    const dependencies = []
-
+  install() {
     this.log('Installing dependencies...')
-
-    this.yarnInstall(devDependencies, { 'dev': true })
-    this.yarnInstall(dependencies)
+    this.spawnCommandSync('npm', ['i', '-dev', 'json-loader', 'esbuild'])
   }
 
-  end () {
-    if (this.props.repo) {
+  end() {
+    if (this.props && this.props.repo) {
       this.spawnCommandSync('git', ['init'])
       this.spawnCommandSync('git', ['add', '--all'])
       this.spawnCommandSync('git', ['commit', '--message', '"Hello, App App!"'])
@@ -119,7 +102,16 @@ class CreateNeocastPlugin extends Generator {
     this.log()
     this.log(chalk.cyan(`Generation of plugin complete... Execute:`))
     this.log(chalk.cyan(`  cd ${this.destinationRoot()}`))
-    this.log(chalk.cyan(`to change directory into your new plugin and start coding!`))
+    this.log(chalk.cyan(`to change directory into your new plugin`))
+    this.log()
+    this.log()
+    this.log(chalk.cyan(`When ready to build an uploadable plugin, execute:`))
+    this.log(chalk.cyan(`  npm run build`))
+    this.log(chalk.cyan(`And look for dist/index.js`))
+    this.log()
+    this.log()
+    this.log(chalk.cyan(`Edit src/index.js to start coding!`))
+    this.log()
     this.log()
   }
 }
--- generators/app/templates/README.md
+++ generators/app/templates/README.md
@@ -5,7 +5,5 @@ This is your NEOCAST Media Player Plugin.
 Write your code in `src/index.js`
 
 When ready to deploy:
-  `webpack`
-and your output will be in `dist/bundle.js`
-
-MOAR Documentation here.
+`npm run build`
+and your output will be in `dist/index.js`
